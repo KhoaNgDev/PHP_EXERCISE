1498c64098e3352aa12e2d6be399f3ea2c91d0c0
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2024-08-08 10:25:34 +0700

    Test Lesson 2 +3

--- _practical-app/lesson_2.php
+++ _practical-app/lesson_2.php
@@ -38,53 +38,7 @@
 		$associativeArray = array("first" => 10, "second" => 20, "third" => 30);
 		?>
 
-		<h3 style="color:red">Step 4: Make an if Statement with elseif and else to finally display string saying, I love PHP:</h3>
-		<?php
-		$language = "PHP";
-
-		if ($language == "Python") {
-			echo "I love Python";
-		} elseif ($language == "JavaScript") {
-			echo "I love JavaScript";
-		} else {
-			echo "I love PHP";
-		}
-		?>
-
-		<h3 style="color:red">Step 5: Make a for loop that displays 10 numbers:</h3>
-		<?php
-		for ($i = 1; $i <= 10; $i++) {
-			echo $i . " ";
-		}
-		?>
-
-		<h3 style="color:red">Step 6: Make a switch Statement that test against one condition with 5 cases:</h3>
-		<?php
-		$day = "Wednesday";
-
-		switch ($day) {
-			case "Monday":
-				echo "Today is Monday";
-				break;
-			case "Tuesday":
-				echo "Today is Tuesday";
-				break;
-			case "Wednesday":
-				echo "Today is Wednesday";
-				break;
-			case "Thursday":
-				echo "Today is Thursday";
-				break;
-			case "Friday":
-				echo "Today is Friday";
-				break;
-			default:
-				echo "It's the weekend!";
-				break;
-		}
-		?>
-
-
+		
 
 
 	</article><!--MAIN CONTENT-->
--- _practical-app/lesson_3.php
+++ _practical-app/lesson_3.php
@@ -1,38 +1,68 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<section class="content">
 
 	<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
+		<?php Navigation(); ?>
+
 	</aside><!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
+	<article class="main-content col-xs-8">
 
-<?php  
+		<h3 style="color:red">Step 1: Make an if Statement with elseif and else to finally display string saying, I love PHP:</h3>
+		<?php
+		$language = "PHP";
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+		if ($language == "Python") {
+			echo "I love Python";
+		} elseif ($language == "JavaScript") {
+			echo "I love JavaScript";
+		} else {
+			echo "I love PHP";
+		}
+		?>
+
+		<h3 style="color:red">Step 2: Make a for loop that displays 10 numbers:</h3>
+		<?php
+		for ($i = 1; $i <= 10; $i++) {
+			echo $i . " ";
+		}
+		?>
+
+		<h3 style="color:red">Step 3: Make a switch Statement that test against one condition with 5 cases:</h3>
+		<?php
+		$day = "Wednesday";
+
+		switch ($day) {
+			case "Monday":
+				echo "Today is Monday";
+				break;
+			case "Tuesday":
+				echo "Today is Tuesday";
+				break;
+			case "Wednesday":
+				echo "Today is Wednesday";
+				break;
+			case "Thursday":
+				echo "Today is Thursday";
+				break;
+			case "Friday":
+				echo "Today is Friday";
+				break;
+			default:
+				echo "It's the weekend!";
+				break;
+		}
+		?>
 
 
 
-	Step 2: Make a forloop  that displays 10 numbers
-
-
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
- */
-
-	
-?>
 
 
 
+	</article><!--MAIN CONTENT-->
 
-
-
-</article><!--MAIN CONTENT-->
-	
-<?php include "includes/footer.php"; ?>
+	<?php include "includes/footer.php"; ?>
