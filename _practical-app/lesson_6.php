7a512dfc1b9ffdbb93639b0653c78dd9b9644dfc
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-08-09 01:38:40 +0700

    updated lessons

--- _practical-app/lesson_6.php
+++ _practical-app/lesson_6.php
@@ -8,22 +8,38 @@
     </aside><!--SIDEBAR-->
 
     <article class="main-content col-xs-8">
+        <!--     <?php
+
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                        $submittedValue = $_POST['value'];
+                        echo '<h3 style="color: red;">Form Submitted</h3>';
+                        echo "Value submitted: " . htmlspecialchars($submittedValue);
+                    }
+                    ?> -->
         <?php
-        // Kiểm tra nếu form đã được gửi
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Lấy giá trị từ form
-            $submittedValue = $_POST['value'];
-            echo '<h3 style="color: red;">Form Submitted</h3>';
-            echo "Value submitted: " . htmlspecialchars($submittedValue);
+
+        if (isset($_POST['submit'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            echo 'Hello' . $username . '<br>';
+            echo 'Your Password is : ' . $password;
         }
         ?>
-
         <!-- Bước 1: Tạo một form gửi một giá trị đến biến toàn cục POST -->
-        <form method="post" action="">
-            <label for="value">Enter a value:</label>
-            <input type="text" id="value" name="value" required>
-            <input type="submit" value="Submit">
+        <form method="post" action="lesson_6.php">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" placeholder="Please Fill The Input">
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Please Fill The Input">
+            <br>
+            <input name="submit" type="submit" value="Submit">
         </form>
+
+
+
+
     </article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php"; ?>
+    <?php include "includes/footer.php"; ?>
