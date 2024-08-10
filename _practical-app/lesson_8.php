7a6d97fb075e2d2958959cf1a1fffc717ba1dbd2
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2024-08-10 12:41:01 +0700

    Task Lesson 8

--- _practical-app/lesson_8.php
+++ _practical-app/lesson_8.php
@@ -4,34 +4,19 @@
 <section class="content">
 
     <aside class="col-xs-4">
-
         <?php Navigation(); ?>
-
-
     </aside><!--SIDEBAR-->
 
-
-
     <article class="main-content col-xs-8">
-
-
         <?php
-
-        /*  Step 1 -Make a variable with some text as value
-
-		Step 2 - Use crypt() function to encrypt it
-
-		Step 3 - Assign the crypt result to a variable
-
-		Step 4 - echo the variable
-
-	*/
-
+        // ? $1$ = md5crypt ( MD5-based cryptography )
+        $text = 'my_password';
+        $salt = '$1$ma_hoa_ban_dau$'; 
+        $encrypted_text = crypt($text, $salt);
+        $encrypted_result = $encrypted_text;
+        echo '<h3 style="color:red">Encrypted text: </h3>' . $encrypted_result;
         ?>
-
-
-
-
-
     </article><!--MAIN CONTENT-->
+
     <?php include "includes/footer.php"; ?>
+</section>
