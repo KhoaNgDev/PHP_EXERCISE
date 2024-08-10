92a73b7a64c2840887b9935618cbdd1afaf0244a
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2024-08-10 12:33:34 +0700

    SQL Injection

--- _practical-app/mysql/functions.php
+++ _practical-app/mysql/functions.php
@@ -1,9 +1,8 @@
 <?php
 
 include "db.php";
-?>
 
-<?php
+// Function to show all data in a select dropdown
 function showAllData()
 {
     global $CONNECTION;
@@ -13,27 +12,23 @@ function showAllData()
         die("MYSQL QUERY FAILED: " . mysqli_error($CONNECTION));
     }
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<option value=" . $row['id'] . ">" . $row['username'] . "</option>";
+        echo "<option value='" . htmlspecialchars($row['id']) . "'>" . htmlspecialchars($row['username']) . "</option>";
     }
 }
+
+// Function to update user data
 function updateTable()
 {
     global $CONNECTION;
 
-    // Kiểm tra sự tồn tại của các trường trong mảng $_POST
     if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['id'])) {
         $username = mysqli_real_escape_string($CONNECTION, $_POST['username']);
         $password = mysqli_real_escape_string($CONNECTION, $_POST['password']);
         $id = mysqli_real_escape_string($CONNECTION, $_POST['id']);
 
-        // Mã hóa mật khẩu
-        $password = password_hash($password, PASSWORD_DEFAULT);
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-        // Câu lệnh SQL
-        $query = "UPDATE users SET ";
-        $query .= "username = '" . mysqli_real_escape_string($CONNECTION, $username) . "', ";
-        $query .= "password = '" . mysqli_real_escape_string($CONNECTION, $password) . "' ";
-        $query .= "WHERE id = '" . mysqli_real_escape_string($CONNECTION, $id) . "'";
+        $query = "UPDATE users SET username = '$username', password = '$passwordHash' WHERE id = '$id'";
         $result = mysqli_query($CONNECTION, $query);
 
         if (!$result) {
@@ -45,17 +40,16 @@ function updateTable()
         echo "Please fill in all fields.";
     }
 }
+
+// Function to delete a user
 function deleteRows()
 {
     global $CONNECTION;
 
     if (isset($_POST['id'])) {
-        $id = $_POST['id'];
+        $id = mysqli_real_escape_string($CONNECTION, $_POST['id']);
 
-        // Câu lệnh SQL
-        $query = "DELETE FROM users ";
-        $query .= "WHERE id = '$id'";
-
+        $query = "DELETE FROM users WHERE id = '$id'";
         $result = mysqli_query($CONNECTION, $query);
 
         if (!$result) {
@@ -68,21 +62,21 @@ function deleteRows()
     }
 }
 
+// Function to create a new user
 function createRows()
 {
     global $CONNECTION;
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $password = password_hash($password, PASSWORD_DEFAULT);
 
-        // Kiểm tra dữ liệu đầu vào
+    if (isset($_POST['submit'])) {
+        $username = mysqli_real_escape_string($CONNECTION, $_POST['username']);
+        $password = mysqli_real_escape_string($CONNECTION, $_POST['password']);
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
         if (empty($username) || empty($password)) {
             die('Please fill out both fields.');
         }
 
-        $query = "INSERT INTO users(username, password)";
-        $query .= "VALUES('$username', '$password')";
+        $query = "INSERT INTO users (username, password) VALUES ('$username', '$passwordHash')";
         $result = mysqli_query($CONNECTION, $query);
 
         if ($result) {
@@ -91,31 +85,27 @@ function createRows()
             die('QUERY FAILED: ' . mysqli_error($CONNECTION));
         }
 
-        // Đóng kết nối
         mysqli_close($CONNECTION);
     }
 }
-function readRows() {
+
+// Function to read and display all users
+function readRows()
+{
     global $CONNECTION;
-    
-    // Câu truy vấn SQL để lấy tất cả các dữ liệu từ bảng 'users'
+
     $query = "SELECT * FROM users";
-    
-    // Thực hiện truy vấn
     $result = mysqli_query($CONNECTION, $query);
-    
-    // Kiểm tra xem truy vấn có thành công không
+
     if (!$result) {
         die("MYSQL QUERY FAILED: " . mysqli_error($CONNECTION));
     }
-    
-    // Duyệt qua từng hàng dữ liệu và in ra dưới dạng HTML
+
     while ($row = mysqli_fetch_assoc($result)) {
-        $id = $row['id'];
-        $username = $row['username'];
-        $password = $row['password'];
-        
-        // In hàng dữ liệu dưới dạng hàng trong bảng HTML
+        $id = htmlspecialchars($row['id']);
+        $username = htmlspecialchars($row['username']);
+        $password = htmlspecialchars($row['password']);
+
         echo "<tr>";
         echo "<th scope='row'>{$id}</th>";
         echo "<td>{$username}</td>";
@@ -124,3 +114,5 @@ function readRows() {
         echo "</tr>";
     }
 }
+
+?>
